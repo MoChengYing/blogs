80474b03d0eae75c203bd08b3be96c4753a9441f
bruce <bruce@qq.com>
2021-04-21 21:05:01 +0800

    'redis配置'

--- blogs/blogs/settings.py
+++ blogs/blogs/settings.py
@@ -111,3 +111,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CACHES={
+    "default":{
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+   "session":{ # session1号库
+    "BACKEND":"django_redis.cache.RedisCache",
+    "LOCATION":"redis://127.0.0.1:6379/1",
+    "OPTIONS":{
+        "CLIENT_CLASS":"django_redis.client.DefaultClient",
+        }
+    },
+}
+# session由数据库存储改为redis存储
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session" # 指定保存到session1号库
---
+++ qidong.bat
@@ -0,0 +1,5 @@
+@echo off
+E:
+cd E:\install_compute\Redis-x64-3.0.504
+redis-server.exe redis.windows.conf 
+pause>end
